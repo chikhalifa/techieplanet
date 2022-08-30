3874b06b737e21dcba3c1a67e98e9d6318655410
chikhalifa <49573718+chikhalifa@users.noreply.github.com>
2022-08-30 08:56:57 +0100

    Update button text

--- studentApp/application/views/register.php
+++ studentApp/application/views/register.php
@@ -32,6 +32,6 @@
       <?php echo form_input(array('name'=>'Chemistry', 'id'=> 'Chemistry', 'placeholder'=>'Chemistry', 'class'=>'form-control', 'value'=> set_value('email'))); ?>
       <?php echo form_error('Chemistry');?>
     </div>
-    <?php echo form_submit(array('value'=>'Sign up', 'class'=>'btn btn-lg btn-primary btn-block')); ?>
+    <?php echo form_submit(array('value'=>'Submit', 'class'=>'btn btn-lg btn-primary btn-block')); ?>
     <?php echo form_close(); ?>
 </div>
